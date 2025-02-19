abb7c972b5df0037cda987b06d47e104eb90a28e
Bhuvan-ops <bhuvan.k@robosoftin.com>
2025-02-19 15:24:52 +0530

    Update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+const appName = process.env.APP_NAME
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`${appName} is listening on port ${port}`);
+});
